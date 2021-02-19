c1470197c09332e6b1459b4d76ed431d5371bd8b
shaobaitao <noob@shaobaitao.cn>
2021-02-19 16:01:58 +0800

    myvue 1.1

---
+++ src/components/test.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <el-switch
+        v-model="value1"
+        active-text="按月付费"
+        inactive-text="按年付费">
+    </el-switch>
+    <el-switch
+        style="display: block"
+        v-model="value2"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-text="按月付费"
+        inactive-text="按年付费">
+    </el-switch>
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="item in 6" :key="item">
+        <h3 class="medium">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+
+    <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        show-arrows-on-hover
+    >
+      <v-carousel-item
+          v-for="(slide, i) in slides"
+          :key="i"
+      >
+        <v-sheet
+            :color="colors[i]"
+            height="100%"
+        >
+          <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+          >
+            <div class="display-3">
+              {{ slide }} Slide
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      value1: true,
+      value2: true,
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+</style>
